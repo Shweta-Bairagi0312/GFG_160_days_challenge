c906c6b721d31f4c60caf45ae3001e810bbb26fb
Shweta-Bairagi0312 <shwetabairagi742@gmail.com>
2025-11-24 20:28:16 +0530

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,18 @@
+//Kadane's Algorithm
+
+class Solution {
+    int maxSubarraySum(int[] arr) {
+       int maxi = Integer.MIN_VALUE;
+       int sum = 0;
+       for(int i = 0; i<arr.length;i++){
+           sum += arr[i];
+           maxi = Math.max(sum, maxi);
+           if(sum<0){
+           sum = 0;
+       }
+           
+       }
+       
+        return maxi;
+    }
+}
