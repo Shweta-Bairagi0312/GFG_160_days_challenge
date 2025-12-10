d643ed5261ce0811e8f5cc6070a7069b7fc37228
Shweta-Bairagi0312 <shwetabairagi742@gmail.com>
2025-12-10 16:57:16 +0530

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Second pass to maintain order
+        for (char ch : s.toCharArray()) {
+            if (map.get(ch) == 1) {
+                return ch;
+            }
+        }
+
+        return '$';
+    }
+}
