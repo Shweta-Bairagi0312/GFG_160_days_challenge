36143ade58535436b18b8c878b4c9ed44379ae1a
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-04-11 22:43:53 +0530

    Implement method to convert binary linked list to decimal

---
+++ day55.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+       int res = 0;
+       ListNode temp = head;
+       while(temp != null){
+        res = res*2+temp.val;
+        temp = temp.next;
+       } 
+       return res;
+    }
+}
