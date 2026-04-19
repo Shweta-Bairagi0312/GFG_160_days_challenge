ac205eaf3df64395ecfccdf4b4fff8cf52dc08b0
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-04-19 22:24:06 +0530

    Implement minimumSum function to calculate sum

---
+++ day65.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumSum(int num) {
+
+        int[] digit = new int[4];
+        int freq = 0;
+
+        while(num>0){
+            digit[freq++] = num%10;
+            num /= 10;
+        }
+        Arrays.sort(digit);
+        int num1 = digit[0]*10+digit[2];
+        int num2 = digit[1]*10+digit[3];
+
+        return num1+num2;
+
+        
+    }
+}
