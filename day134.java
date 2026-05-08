effdac8f82bf2d41c4ac05668ebe92de15770d5a
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-05-08 17:28:44 +0530

    Add graph implementation with pathfinding method
    Implemented a graph structure and a method to find all paths from a source to a destination.

---
+++ day134.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class hasPath{
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w){
+        this.src = s;
+        this.dest = d;
+        this.wt = w;
+    }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,1));
+        graph[0].add(new Edge(0,2,1));
+
+        graph[1].add(new Edge(1,0,1));
+        graph[1].add(new Edge(1,3,1));
+
+        graph[2].add(new Edge(2,0,1));
+        graph[2].add(new Edge(2,4,1));
+       
+        graph[3].add(new Edge(3,1,1));
+        graph[3].add(new Edge(3,4,1));
+        graph[3].add(new Edge(3,5,1));
+
+        graph[4].add(new Edge(4,2,1));
+        graph[4].add(new Edge(4,3,1));
+        graph[4].add(new Edge(4,5,1));
+
+        graph[5].add(new Edge(5,3,1));
+        graph[5].add(new Edge(5,4,1));
+        graph[5].add(new Edge(5,6,1));
+        graph[5].add(new Edge(5,5,1));
+      
+
+
+    }
+
+    public static void allPath(ArrayList<Edge>graph[],int src,int dest,String path){
+        if(src==dest){
+            System.out.println(path+dest);
+            return;
+        }
+        for(int i = 0; i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            allPath(graph,e.dest,dest, path+src);
+
+        }
+    }
+
+    public static void main(String[] args){
+        int V = 7;
+        ArrayList<Edge>graph[] = new ArrayList[V];
+        createGraph(graph);
+        int src = 5;
+        int dest = 1;
+        String path = " ";
+        allPath(Agraph[],srcdest,path);
+     
+        
+    }
+    
+}
