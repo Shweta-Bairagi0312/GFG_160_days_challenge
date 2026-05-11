f41c62a80c26ff0a24cda1380a3a4e2463091837
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-05-11 19:54:13 +0530

    Implement quicksort algorithm in Divideconquer class

---
+++ day155.java
@@ -0,0 +1,49 @@
+class Divideconquer {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int pindx = partition(arr, si, ei);
+        quickSort(arr, si, pindx - 1); // left part
+        quickSort(arr, pindx + 1, ei); // right part
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        i++;
+        int temp = arr[ei];
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6, 3, 9, 8, 2, 5};
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
+
+
